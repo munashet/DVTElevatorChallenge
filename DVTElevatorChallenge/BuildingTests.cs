63c272fdb53b919deea1534f7c5df8db3c012337
Munashe Tsododo <munashet@outlook.com>
2024-04-20 13:54:43 +0200

    Added Unit Tests

---
+++ DVTElevatorChallenge/BuildingTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class BuildingTests
+{
+    [Test]
+    public void CallElevator_MovesElevatorToRequestedFloor()
+    {
+        Building building = new Building(10, 1);
+        building.CallElevator(5, 3);
+        Assert.Equals(5, building.Elevators[0].CurrentFloor);
+    }
+
+    [Test]
+    public void CallElevator_ThrowsExceptionWhenNoAvailableElevator()
+    {
+        Building building = new Building(10, 1);
+        building.Elevators[0].AddPassengers(10);
+        Assert.Throws<InvalidOperationException>(() => building.CallElevator(5, 3));
+    }
+}
---
+++ DVTElevatorChallenge/ElevatorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class ElevatorTests
+{
+    [Test]
+    public void MoveToFloor_ChangesCurrentFloorAndDirection()
+    {
+        Elevator elevator = new Elevator();
+        elevator.MoveToFloor(5);
+        Assert.Equals(5, elevator.CurrentFloor);
+        Assert.Equals(Direction.Up, elevator.Direction);
+    }
+
+    [Test]
+    public void AddPassengers_IncreasesPassengerCount()
+    {
+        Elevator elevator = new Elevator();
+        elevator.AddPassengers(3);
+        Assert.Equals(3, elevator.PassengerCount);
+    }
+
+    [Test]
+    public void AddPassengers_ThrowsExceptionWhenOverCapacity()
+    {
+        Elevator elevator = new Elevator { MaxPassengerCount = 5 };
+        Assert.Throws<InvalidOperationException>(() => elevator.AddPassengers(6));
+    }
+}
